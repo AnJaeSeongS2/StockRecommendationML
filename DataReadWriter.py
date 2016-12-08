be898c9c51afc27b9852aca2c1bdb621fe9d8ebf
AnJaeSeongS2 <jindam91@naver.com>
2016-12-09 07:26:23 +0900

    It is available that reading, writing Direction with DB

--- DataReadWriter.py
+++ DataReadWriter.py
@@ -56,7 +56,6 @@ class DataReader:
 
 		return pd.concat([seriesCode,seriesCompany], axis=1)
 		
-	
 	#return dataFrame_prices from db
 	def loadPrices(self, code, limit=0):
 		sqlGetCode = "select * from prices"	
@@ -94,8 +93,147 @@ class DataReader:
 	
 		return pd.concat([seriesDate, seriesCode, seriesOpen, seriesClose, seriesLow, seriesHigh, seriesAdjClose, seriesVolume], axis=1)
 		
+	
+	#return dataFrame_directions from db
+	def loadDirections(self, date, limit=0):
+		sqlGetCode = "select * from directions"	
+		sqlGetCode += " where price_date = \"%s\""%(date+" 00:00:00")
+		if( limit !=0) :
+			sqlGetCode += " limit %s;"%limit
+		else :
+			sqlGetCode += ";"
+		self.cursor.execute(sqlGetCode)
+		rows = self.cursor.fetchall()
+        #self.db.commit()
 		
+		seriesDate = pd.Series(name='date')
+		seriesCode = pd.Series(name= 'code')
+		seriesCompany = pd.Series(name='company')
+		seriesTargetColumn = pd.Series(name= 'target_column')
+		seriesDirection = pd.Series(name='direction')
+		
+		for a_index in range(0,len(rows)):
+			seriesDate = seriesDate.set_value(a_index,rows[a_index][1])
+			seriesCode = seriesCode.set_value(a_index,rows[a_index][2])
+			seriesCompany= seriesCompany.set_value(a_index,rows[a_index][3])
+			seriesTargetColumn = seriesTargetColumn.set_value(a_index,rows[a_index][4])
+			seriesDirection = seriesDirection.set_value(a_index,rows[a_index][5])
+		
+		#print seriesCode.to_frame().iloc[0]['code']
+		#self.db.close()
+	
+		return pd.concat([seriesDate, seriesCode, seriesCompany, seriesTargetColumn, seriesDirection],axis=1)
+		
+class DataWriter:
+	db = 0
+	cursor = 0
+
+	#Naming Mangling 으로 외부에서 파악 불가능하게 함.
+	__insCount = 0
+		
+	def __init__(self):
+		DataWriter.__insCount +=1
+		# 디비 연결
+		self.db = MySQLdb.connect("localhost","dev","aA99426383","StockRecommendationML" )
+		self.db.set_character_set('utf8')
+		# prepare a cursor object 
+		self.cursor = self.db.cursor()
+		self.cursor.execute('SET NAMES utf8;')
+		self.cursor.execute('SET CHARACTER SET utf8;')
+		self.cursor.execute('SET character_set_connection=utf8;')
+		
+	def __del__(self):
+		self.db.close()
+
+	def instanceCount(cls):
+		print ("DataReader Instance Count : %s" % cls.__insCount)
+		return cls.__insCount
 	
 
+
+
+	#if this func didn't work, use module DataWriter
+	def updateCodesToDB(self):
+		try:
+			# INSERT
+			# download Stock's Code from Koscom
+			print "Starting Downloading Stock Codes from Koscom."
+			codes =StockCode.StockCode()
+			codes.parseCodeHTML(codes.downloadCode(1),1)
+			codes.parseCodeHTML(codes.downloadCode(2),2)
+
+			sqlAddCode = """insert into codes (last_update, code, full_code, market_type, company) values (now(),%s,%s,%s,%s)"""
+			for key, value in codes.iterItems():
+				if( len(key) == 0 ):
+					continue
+				dataCode = (key, value.full_code,value.market_type, value.company)
+				self.cursor.execute(sqlAddCode, dataCode)	
+			self.db.commit()
+			
+		finally:
+			self.db.close()
+
+	#if this func didn't work, use module DataWriter			
+	def updatePricesToDB(self,market_type, dateTime1, dateTime2,start_id = 0):
+		try:
+			# INSERT
+			# downlodat Stock's data from Yahoo
+			print "Starting Downloading Stock Data from Yahoo. market_type: %s , %s ~ %s" % (market_type, dateTime1.isoformat() , dateTime2.isoformat() )
+			sqlGetCode = "select * from codes"
+			sqlGetCode += " where market_type =%s" % (market_type)
+			if start_id  >0 :
+				sqlGetCode +=" and id >%s" %(start_id)
+			self.cursor.execute(sqlGetCode)
+			rows =self.cursor.fetchall()
+			#cursor.beginTrans()
+			print len(rows)
+			
+			index = 1
+			for a_row in rows:
+				code = a_row[2]
+				company = a_row[5]
+				#data_count = db.getDataCount(code)
+				#if data_count == 0:
+				print "... %s of %s : Downloading %s 's data " % (index, len(rows), company)
+					
+				file_name = 'stockData/(%s)' %(index)
+				file_name += a_row[5]
+				file_name += ".data"
+				print 'filename : %s , company : %s , code : %s' %(file_name, company, code)
+				df_data =StockPrice.downloadStockData(file_name, code, dateTime1, dateTime2)
+				if df_data is not None:
+					#DB로 전송
+					for i in range(0,len(df_data.index)):
+						if(df_data.Volume[i] != 0): #주식 거래량이 0이 아닌 것만  DB갱신
+							sqlAddPrice = """insert into prices (last_update, price_date, code, price_open, price_close, price_low, price_high, price_adj_close, volume) values (now(), %s, %s, %s, %s, %s, %s, %s , %s)"""
+							dataPrice = (df_data.index[i].strftime('%Y-%m-%d %H:%M:%S'), a_row[2], df_data.Open[i], df_data.Close[i], df_data.Low[i], df_data.High[i], df_data.iloc[i]['Adj Close'], df_data.Volume[i] )
+							self.cursor.execute(sqlAddPrice, dataPrice)
+				# end if df_data is not None
+				index+=1
+				self.db.commit()
+				print "... %s of %s : %s 's data Committed to DB" % (index, len(rows), company)
+			# end for a_row in rows
+		
+			self.db.commit()
+		finally:
+			print "End data Crwaling"
+		
+
+	def updateDirectionsToDB(self,df_directions):
+		try:
+			# INSERT
+			#DB로 전송
+			for i in range(0,df_directions.shape[0]):
+				sqlAddDirection = """insert into directions (last_update, price_date, code, company, target_column, direction) values (now(), %s, %s, %s, %s , %s) ON DUPLICATE KEY UPDATE last_update = now(), target_column= %s, direction = %s"""
+				dataDirection = ( (df_directions.iloc[i]['price_date']+' 00:00:00'), df_directions.iloc[i]['code'], df_directions.iloc[i]['company'],df_directions.iloc[i]['target_column'], df_directions.iloc[i]['direction'] ,df_directions.iloc[i]['target_column'], df_directions.iloc[i]['direction']  )
+				print dataDirection
+				self.cursor.execute(sqlAddDirection, dataDirection)
+				self.db.commit()
+				print "... %s of %s : %s 's direction data Committed to DB" % (i+1, df_directions.shape[0], df_directions.iloc[i]['company'])
+		
+			self.db.commit()
+		finally:
+			print "End data Crwaling"
+		
 
 
--- test_samsungShow.py
+++ test_samsungShow.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python
 
 import pandas as pd
-from  DataReadWriter import DataReader
+from  DataReadWriter import DataReader, DataWriter
 from PortfolioBuilder import PortfolioBuilder
 import numpy as np
 #print pd.read_pickle('samsung2.data')['Close'].pct_change()
@@ -54,28 +54,41 @@ testPercentile = np.percentile( testPrices['price_close'] , np.arange(0,100,10))
 print testPercentile
 print len(testcodes), len(testPrices)
 '''
-
+'''
+# test dictionary to dataframe
+test_dict = {'a':[], 'b':[]}
+test_dict['a'].append(1)
+test_dict['b'].append(2)
+test_dict['a'].append(11)
+test_dict['b'].append(22)
+print test_dict
+print 'adfadsfadsfadsfdsfasdfkjnasndflandslfnadslfmal;sdmf;aldsmf %s '%test_dict['a'][0]
+print pd.DataFrame(test_dict)
+'''
 
 portfolio = PortfolioBuilder()
 #해당날짜의 평균회귀 성향 확인
 current_date = '2015-11-20'
 target_column = 'price_close'
-df_mean_reversion = portfolio.doMeanReversionTest('price_close',current_date,100,30)
+df_mean_reversion = portfolio.doMeanReversionTest('price_close',current_date,100,10)
 df_rank = portfolio.rankMeanReversion(df_mean_reversion)
-mean_reversion_codes = portfolio.buildUniverse(df_rank,'rank',0.8)
+mean_reversion_codes = portfolio.buildUniverse(df_rank,'rank',0.0)
 #해당날짜의 평균회귀 성향이 높은 주식들만 방향 예측
-mean_reversion_direction = {'date':[],'code':[],'company':[],'direction':[]}
+mean_reversion_direction = {'price_date':[],'code':[],'company':[],'target_column':[],'direction':[]}
+dw = DataWriter()
+dr = DataReader()
+print dr.loadDirections(current_date)
+
 for index in range(len(mean_reversion_codes)):
-	mean_reversion_direction['date'].append(current_date)
+	mean_reversion_direction['price_date'].append(current_date)
 	mean_reversion_direction['code'].append(mean_reversion_codes['code'].iloc[index])
 	mean_reversion_direction['company'].append(mean_reversion_codes['company'].iloc[index])
+	mean_reversion_direction['target_column'].append(target_column)
 	mean_reversion_direction['direction'].append(portfolio.determineMeanReversionDirection(mean_reversion_codes['code'].iloc[index], target_column, current_date, verbose=True) )
-	
-	
-	print 'date: %s, code: %s, company: %s, direction: %s'%(mean_reversion_direction.iloc[index]['date'], mean_reversion_direction.iloc[index]['code'], mean_reversion_direction.iloc[index]['company'], mean_reversion_direction.iloc[index]['direction'], )
-	
 
-
+	print 'price_date: %s, code: %s, company: %s, column: %s, direction: %s'%(mean_reversion_direction['price_date'][index], mean_reversion_direction['code'][index], mean_reversion_direction['company'][index], mean_reversion_direction['target_column'][index], mean_reversion_direction['direction'][index] )
+df_mean_reversion_direction = pd.DataFrame(mean_reversion_direction)
+dw.updateDirectionsToDB(df_mean_reversion_direction)
 
 '''
 testC = {'2010-01-04':'1234' ,'2011-01-01':'2345', '2013-02-02':'4444'}
